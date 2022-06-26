64346ee59da6a97f6ca2d8811f96d62969d2d426
allanstar-byte <allanmutisya263@gmail.com>
2022-06-26 11:06:43 +0300

    final trial

---
+++ Readme.md
@@ -0,0 +1,50 @@
+# ChefCula
+
+Welcome to my space friend !!!
+
+I'm ChefCula and I will help you learn new gems in your kitchen.
+
+My dedication is to help lovers of good food share their super recipes to the world. Wait! And bring us together. 
+
+My developer gave me a male name and I love it 
+
+## Version
+I'm currently on ccv1 (ccv1 are the naming of the application versions meaning chefcula version 1). I'll continue building him until he is the best.
+
+## Technologies Used
+
+He is currently being build using
+
+    1. JavaScript (ES6)
+    
+    2. HTML
+
+    3. CSS
+
+## Contributors
+
+He is currently being build by Allan Ngoma
+
+## Setup/Installation Requirements
+
+Clone the repo {git clone https://github.com/allanstar-byte/ChefCula.git}
+
+Ensure to run your server before going live  {json-server --watch db.json }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+copyright &copy; allan Ngoma
---
+++ index.js
@@ -0,0 +1,72 @@
+// silencing the default features - reloading
+
+document.addEventListener('DOMContentLoaded', (stopDefault) => {
+    stopDefault.preventDefault()
+    console.log(stopDefault.target)
+})
+
+// All renders
+
+function renderAll(get1) {
+
+    let card = document.createElement("ul")
+    card.innerHTML =
+        `
+    <p>${get1.description} - <span> ${get1.source}</span></p> <br>
+    `
+    document.querySelector("#populate").appendChild(card)
+
+}
+
+
+function renderGal(get2) {
+
+    let gal = document.createElement('p')
+        //  GAllery
+    gal.innerHTML =
+        `
+        <div class = "galShow">
+            <img src=${get2.thumbnail} alt="">
+        
+            <p>${get2.description}</p>
+        </div>
+        `
+
+    document.querySelector(".list").appendChild(gal)
+}
+
+
+//  Fetching all dishes
+function getAllQuotes() {
+    fetch('http://localhost:3000/quotes')
+        .then(res => res.json())
+        .then(dishData => dishData.forEach(get1 => renderAll(get1)))
+}
+
+function getAllGal() {
+    fetch('http://localhost:3000/gallery')
+        .then(res => res.json())
+        .then(dishData => dishData.forEach(get2 => renderGal(get2)))
+}
+
+
+
+
+// All initializations
+
+function initialize() {
+    getAllQuotes()
+    getAllGal()
+
+}
+
+initialize()
+
+// function myFunction() {
+//     var x = document.getElementById("myDIV");
+//     if (x.style.display === "none") {
+//         x.style.display = "block";
+//     } else {
+//         x.style.display = "none";
+//     }
+// }
